b7dd2e7751187421278ed6a1246549b8a8d98dcf
matusita syuntarou <e16003@std.it-college.ac.jp>
2016-09-08 21:49:50 +0900

    一覧表示用のテンプレートを追加

--- index.php
+++ index.php
@@ -1,63 +1,48 @@
 <?php get_header(); ?>
 
-<div class="contentsWrap">
-  <div class="mainContents">
+    <div class="contentsWrap">
+        <div class="mainContents">
 
-    <div class="aboutBlock block">
-      <div class="banners">
-        <ul>
-          <li><a href="about.html">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/home/bnr_about.png" height="97" width="320" alt="ホテル紹介"></a></li>
-          <li><a href="access.html">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/home/bnr_access.png" height="97" width="320" alt="アクセス"></a></li>
-        </ul>
-      </div>
-    </div><!-- /.aboutBlock -->
+          <?php if ( is_month() ): ?>
+            <h1 class="type-A"><?php the_time('Y年m月'); ?></h1>
+          <?php else: ?>
+            <h1 class="type-A"><?php wp_title(''); ?></h1>
+          <?php endif; ?>
 
-    <section class="newsBlock block">
-      <h1 class="type-B"><span>新着情報</span></h1>
-  <?php
-     if ( have_posts() ) :
-      while ( have_posts() ) : the_post();
-    ?>
 
-      <article id="post-<?php the_ID(); ?>" <?php post_class('news'); ?>>
-        <div class="text">
-          <div class="entryInfo">
-            <div class="categories">
-            <?php the_category(); ?>
-            </div>
-            <time datetime="<?php the_time('Y-m-d'); ?>">
-            <?php the_time('Y年m月d日(l)'); ?></time>
-          </div>
-          <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-          <?php the_excerpt(); ?>
-          <p>
-          </p>
-          <p>[<a href="<?php the_permalink(); ?>">続きを読む</a>]</p>
-        </div>
-        <figure>
-        <?php if ( has_post_thumbnail() ): ?>
-        <a href="<?php the_permalink(); ?>" ?><php the_post_thumbnail('thumbnail'); ?></a>
-      <?php else: ?>
-        <a href="<?php the_permalink(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/common/noimage_180x180.png" height="180" width="180"alt=""></a>
-      <?php endif ; ?>
-        </figure>
-      </article><!-- /.news -->
+            <section class="newsBlock block">
 
-<?php
-endwhile;
-endif;
-?>
+                <article class="news">
+                    <div class="text">
+                        <div class="entryInfo">
+                            <div class="categories">
+                                <ul>
+                                    <li><a href="#">コラム</a></li>
+                                </ul>
+                            </div>
+                            <time datetime="2015-02-14">2015.02.14(sat)</time>
+                        </div>
+                        <h1><a href="single.html">バレンタイン企画を行います</a></h1>
+                        <p>
+                            本日はバレンタインデーですね。当ホテルでもバレンタイン企画を行います。
+                            ディナーをご予約されたお客さま限定で特別コースをご提供させて頂きます。
+                        </p>
+                        <p>[<a href="single.html">続きを読む</a>]</p>
+                    </div>
+                    <figure><a href="single.html"><img src="images/dummy/180x180-2.png" height="180" width="180" alt=""></a></figure>
+                </article><!-- /.news -->
 
-    </section><!-- /.newsBlock -->
+            </section><!-- /.newsBlock -->
+        </div><!-- /.mainContents -->
 
-  </div><!-- /.mainContents -->
+        <aside class="subContents">
+            <div class="wrapper">
 
-  <aside class="subContents">
-  <?php get_sidebar(); ?>
+          <?php get_sidebar('categories'); ?>
+          <?php get_sidebar('archives'); ?>
 
-  </aside><!-- /.subContents -->
-</div><!-- /.contentsWrap -->
+            </div><!-- /.wrapper -->
+        </aside><!-- /.subContents -->
+    </div><!-- /.contentsWrap -->
 
-<?php get_footer(); ?>
+  <?php get_footer(); ?>
