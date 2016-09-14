66f473c37608d5d5241d0d823abeb34d32822ed9
matusita syuntarou <e16003@std.it-college.ac.jp>
2016-09-14 21:53:31 +0900

    BrandcrimnavXTプラグインを使用して、パンくずリスト表示

--- header.php
+++ header.php
@@ -45,3 +45,14 @@ wp_head();
       ?>
 
     </nav><!-- /.globalNavi -->
+
+    <?php if ( !is_home() ): ?>
+      <div class="breadcrumbs">
+      <?php
+      //パンくずリストを表示
+      if ( function_exists( 'bcn_display')){
+        bcn_display();
+      }
+       ?>
+       </div>
+     <?php endif; ?>
