c3731ebcb6ea12bdfc320a42f2e7accd950b8f15
matusita syuntarou <e16003@std.it-college.ac.jp>
2016-09-14 19:36:26 +0900

    サイト内検索

---
+++ searchform.php
@@ -0,0 +1,1 @@
+<form action="<?php echo home_url('/'); ?>" method="get" class="searchform"><input type="text" value="<?php the_search_query(); ?>" name="s" id="s" placeholder="サイト内検索"><input type="submit" id="searchSubmit" value=""></form>
