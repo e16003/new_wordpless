2973900e4105fd41d569f9c33574d312eb60f66e
matusita syuntarou <e16003@std.it-college.ac.jp>
2016-09-15 22:02:48 +0900

    9/15

--- functions.php
+++ functions.php
@@ -8,3 +8,16 @@ add_theme_support( 'post-thumbnails' );
 * カスタムメニュー機能を使用する
 */
 add_theme_support('menus' );
+
+add_filter('comment_form_default_fields','my_comment_form_default_fields');
+function my_comment_form_default_fields( $args ){
+$args['author'] = ''; //「名前」を削除
+$args['email'] = ''; //「メールアドレス」を削除
+$args['url'] ='';//「ウェブサイト」を削除
+return $args;
+}
+
+/**
+*head内にRSSのlink要素を出力する
+*/
+add_theme_support('sutomatic-feed-links');
