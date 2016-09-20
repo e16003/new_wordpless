4a9cc00b9936584ba54ae108f451d21d3716b126
matusita syuntarou <e16003@std.it-college.ac.jp>
2016-09-20 20:40:11 +0900

    RSS関連のカスタマイズ

--- functions.php
+++ functions.php
@@ -21,3 +21,37 @@ return $args;
 *head内にRSSのlink要素を出力する
 */
 add_theme_support('sutomatic-feed-links');
+
+/**
+*head内にRSSのlink要素を出力する
+*/
+add_filter('excerpt_mblength','my_excerpt_mlength');
+function my_excerpt_mlength($length){
+  return 50;
+}
+
+/**
+*残りの部分がある旨の表示を変更
+*/
+
+add_filter('excerpt_more','my_excerpt_more');
+function my_excerpt_more($more){
+  return $more. '...';
+}
+
+/**
+*アイキャッチを表示
+*/
+
+function rss_post_thumbnail( $content) {
+    global $post;
+    if (has_post_thumbnail( $post->ID)) {
+        $content = '<p>' . get_the_post_thumbnail($post->ID) .'</p>' . $content;
+    }
+    return $content;
+}
+add_filter( 'the_excerpt_rss',  'rss_post_thumbnail');
+add_filter( 'the_content_feed', 'rss_post_thumbnail');
+
+//RSS 2.0を停止
+remove_action('do_feel_rss2','do_feel_rss2', 10, 1);
