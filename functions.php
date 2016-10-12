766de0e1d67f88fa74269df5c930b34e29c66b1b
matusita syuntarou <e16003@std.it-college.ac.jp>
2016-10-12 22:00:52 +0900

    ショートコード、分割関数追加

--- functions.php
+++ functions.php
@@ -90,7 +90,91 @@ function print_admin_stylesheet() {
 * 必ずビジュアルモードは表示されるように設定
 */
 
+
+
 add_filter('wp_default_editor', 'my_wp_default_editor');
 function my_wp_default_editor(){
   return 'tinymce';
+}
+
+/**
+* メッセージを表示する簡単なショートコード
+*/
+
+function shortcode_test(){
+  return "「ショートコードのテストです。」";
+}
+
+add_shortcode('test','shortcode_test');
+
+
+/**
+* ツイッターへのリンクを表示するショートコード
+*/
+
+function shortcode_twitter(){
+
+  return 'こんにちは！ナカシマ(<a href="https://twitter.com/
+  kanakogi" target="_blac">@kanakogi</a>)です。';
+}
+add_shortcode('twitter','shortcode_twitter');
+
+/**
+* パラメータでりんごの個数を受け取るショートコード
+*/
+
+function shortcode_apple(){
+  $atts = shortcode_atts(array(
+      'num' => 5, // $numの初期値を設定
+      ),$atts);
+  extract($atts);   //連想配列から変数を作成
+  return "リンゴが" . $num . "個ありました。";
+  }
+  add_shortcode('apple', 'shortcode_apple');
+
+/**
+* クラス名がwrapの<div>タグで囲むショートコード
+*/
+function shortcode_price($atts, $content = null){
+  return '<div class="wrap"><em>価格</em>:' . $content . '</div>';
+}
+add_shortcode('price', 'shortcode_price');
+
+/**
+*  画像データへのパスを返すショートコード
+*/
+function shortcode_url(){
+  echo get_template_directory_uri();
+}
+add_shortcode('dir_url','shortcode_url');
+
+/**
+* アイキャッチ画像を表示する処理を関数化
+*/
+function display_thumbnail(){
+
+  if(has_post_thumbnail() ){
+
+     echo   '<a href="<?php the_permalink(); ?>">' .
+     the_post_thumbnail('thumbnail') . '</a>';
+      }else{
+      echo  '<a href="' .get_permalink() . '"><img src="' . get_template_directory_uri() . '/images/common/noimage_180x180.png" height="180" width="180"alt=""></a>'
+      ;
+    }
+  }
+
+/**
+* 宿泊プランの個別記事の画像を表示する処理を関数化
+*/
+
+function display_image($field_name, $size = 'medium'){
+   $image = get_field($field_name);
+   if(!empty($image)){
+           $url = $image['sizes']['$size'];//中サイズ画像のURL
+           $width = $image['sizes'][$size. '-width'];//中サイズ画像の横幅
+           $height = $image['sizes'][$size.'-height'];//中サイズ画像の縦幅
+
+  echo '<img src="'. $url .'"height="'. $height .'" width="'. $width .'" />';
+     }
+
 }
